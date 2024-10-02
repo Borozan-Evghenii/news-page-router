bf67974d6415f06e8d874c21c38ffb51115edc8f
Borozan-Evghenii <borozan.eugen@gmail.com>
2024-10-02 20:48:24 +0300

    test

--- utils/api/instance.ts
+++ utils/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: process.env.API_BASE_URL,
   headers: {
     Authorization: process.env.API_TOKEN
